94a0c579b37cfb1473d0ac4318b4d3d562f6f06b
NAGOL03 <logan.adams2603@gmail.com>
2024-06-22 14:23:38 -0700

    Section-2 Exercises completed

---
+++ Exercises/Section-2/Exercise-10/test.c
@@ -0,0 +1,4 @@
+#include <stdio.h> 
+int main(void){int height=8,length=12,width=10,volume=height*length*width,weight=(volume+165)/166;
+printf("Dimensions: %dx%dx%d\n",length,width,height);printf("Volume (cubic inches): %d\n",volume);
+printf("Dimensional weight (pounds): %d\n",(volume+165)/166);return 0;}
---
+++ Exercises/Section-2/Exercise-4/Exercise4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int num1, num2, num3;
+float f1, f2, f3;
+
+int main(void) {
+    printf("%d %d %d\n", num1, num2, num3);
+    printf("%f %f %f", f1, f2, f3);
+
+    return 0;
+}
